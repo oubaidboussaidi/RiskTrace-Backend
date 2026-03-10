9d56bb398ccc2919141de3e9a1483899e4068423
Oubaid Boussaidi <oubaydboussaidi@gmail.com>
2026-03-10 16:30:00 +0100

    feat(user): Create org controllers and wire user controller updates

--- user-service/src/main/java/com/risktrace/user_service/Controller/UserController.java
+++ user-service/src/main/java/com/risktrace/user_service/Controller/UserController.java
@@ -8,7 +8,6 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
-import java.security.Principal;
 import java.util.List;
 import java.util.Map;
 
